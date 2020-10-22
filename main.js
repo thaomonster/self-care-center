e8fd66616383917d6b7b73854a7eb39670488a4a
Thao Ma <MaThao@gmail.com>
2020-10-22 15:09:06 -0600

    Add two functions to randomized and generate message

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+var radioBtns = document.querySelector('#radio-button');
+var displayMessage = document.querySelector('#show-message');
+var recieveMessageBtn = document.querySelector('#recieve-message')
+var mantraImg = document.querySelector('.mantra-img')
+
+recieveMessageBtn.addEventListener('click', generateMessage);
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+};
+
+function getSelectedInput() {
+  var selected;
+  radioBtns.forEach(function(btn) {
+    if (btn.checked) {
+      selected = btn.value
+    };
+  });
+  return selected
+};
+
+function generateAffirmation() {
+  var randomAffirmation = affirmation[getRandomIndex(affirmation)];
+
+  displayMessage.insertAdjacentHTML('afterbegin', 
+ `<div>
+  <p>${randomAffirmation}</p>
+  </div>`
+  );
+
+  mantraImg.classList.add('hidden')
+};
+
+function generateMantra() {
+  var randomMantra = mantra[getRandomIndex(mantra)];
+
+  displayMessage.insertAdjacentHTML('afterbegin',
+  `<div>
+  <p>${randomMantra}</p>
+  </div>`
+  );
+
+  mantraImg.classList.add('hidden')
+};
+
+
+
+function toggleImg(elementOne, elementTwo) {
+  elementOne.classList.toggle('hidden');
+  elementTwo.classList.toggle('hidden');
 };
